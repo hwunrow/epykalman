7a1fdeb57002481e0ece1d0aaea77c303bb10a99
Han Yong Wunrow <nhw2114@columbia.edu>
2023-07-27 16:45:25 -0400

    rename inflation and model mcmc scripts

---
+++ src/inflation.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def _adaptive_inflation(x, y, z, oev, lambar_prior=1.1, siglam2=0.1):
+    # step 3b
+    sig2p = np.var(y)
+    ybarp = np.mean(y)
+    D = np.abs(ybarp - z) * z / 50
+
+    # step 3c
+    r = np.cov(x, y)[0, 1]  # i
+    lam0 = (1.0 + r * (np.sqrt(lambar_prior) - 1.0)) ** 2  # iii
+    theta = np.sqrt(lam0 * sig2p + oev)  # iv
+
+    # v/ Appendix A
+    lbar = 1.0 / (np.sqrt(2 * np.pi) * theta) * np.exp(-(D**2) / (2.0 * theta**2))
+    dthetadlam = (
+        sig2p
+        * r
+        * (1.0 - r + r * np.sqrt(lambar_prior))
+        / (2.0 * theta * np.sqrt(lambar_prior))
+    )
+    lprime = lbar * (D**2 / theta**2 - 1.0) / theta * dthetadlam
+
+    b = lbar / lprime - 2 * lambar_prior
+    c = lambar_prior**2 - siglam2 - lbar * lambar_prior / lprime
+
+    lam1 = np.abs((-b + np.sqrt(b**2 - 4.0 * c)) / 2)
+    lam2 = np.abs((-b - np.sqrt(b**2 - 4.0 * c)) / 2)
+
+    if np.abs(lam1 - lambar_prior) < np.abs(lam2 - lambar_prior):
+        return lam1
+    else:
+        return lam2
+
+
+def _inflate_ensembles(ens, inflation_value, params=False):
+    if params:
+        _, m = np.asarray(ens).shape
+        ens_mean = np.mean(ens, 1, keepdims=True)
+        ens_inflated = ens_mean * np.ones((1, m)) + inflation_value * (
+            ens - ens_mean * np.ones((1, m))
+        )
+    else:
+        m = len(ens)
+        ens_mean = np.mean(ens)
+        ens_inflated = ens_mean * np.ones(m) + inflation_value * (
+            ens - ens_mean * np.ones(m)
+        )
+
+    return ens_inflated
