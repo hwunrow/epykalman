884bed19c6fe3230a088e2607246fcb144e2dbd6
Han Yong Wunrow <nhw2114@columbia.edu>
2023-10-25 16:50:11 -0400

    generate data parallel script init

--- src/epyfilter/c2b2/generate_data.py
+++ src/epyfilter/c2b2/generate_data.py
@@ -1,17 +1,34 @@
-import math
-
+import os
+import argparse
 import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-
-from tqdm import tqdm
-
-import importlib
-import sys
-import os 
-sys.path.insert(0, os.path.abspath(os.path.join('../src/')))
-import simulate_data
-importlib.reload(simulate_data)
+from epyfilter import simulate_data
 
 
+if __name__ == '__main__':
+    sge_task_id = os.environ.get("SGE_TASK_ID")
+
+    parser = argparse.ArgumentParser(
+        description="Generate synthetic data.",
+    )
+    parser.add_argument(
+        "--in-dir", type=str, required=True,
+        help="The version the migration data are saved under.")
+    parser.add_argument(
+        "--out-dir", type=str, required=True,
+        help="The version the model results are saved under.")
+    parser.add_argument(
+        "--model-name", type=str, required=True,
+        help="The name of the model.")
+    args = parser.parse_args()
+
+    np.random.seed(1994)
+
+    df = pd.read_csv(os.path.join(args.in_dir, "param_list.csv"))
+    params = df.iloc[sge_task_id - 1].to_dict()
+    data = simulate_data.simulate_data(
+        **params, add_noise=True, noise_param=1/50)
+
+    data.plot_all(path=args.out_dir, name=f"{sge_task_id}_synthetic_plots")
+    data.compute_data_distribution()
+    data.save_data(path=args.out_dir, name=f"{sge_task_id}_synthetic_data")
