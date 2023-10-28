8d98d0c3e0858fb9c93ad2bbda881200d80a5226
Han Yong Wunrow <nhw2114@columbia.edu>
2023-10-28 13:52:33 -0400

    rmse for data and for rt (entire time series and at peaks)

--- src/epyfilter/posterior_checks.py
+++ src/epyfilter/posterior_checks.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def check_param_in_ci(kf, day, percentile=95):
     assert isinstance(day, int), "day must be integer"
     post_betas = np.asarray([θ.beta for θ in kf.θ_list])
@@ -139,3 +138,20 @@ def wasserstein2(p_sample, q_sample, num_bins=10):
     w2 = np.sqrt(np.sum(squared_distances))
 
     return w2
+
+def data_rmse(kf):
+    i_kf = np.array([x.i for x in kf.x_list])
+    rmse = np.sqrt(np.mean((i_kf.T - kf.data.i[1:len(i_kf)+1])**2, axis=0))
+    spread = np.sqrt(np.var(i_kf, axis=1))
+
+    return np.mean(rmse)
+
+def rt_rmse(kf, peaks=None):
+    if peaks is not None:
+        rt_kf = np.array([θ.beta * θ.t_I for θ in kf.θ_list])
+        rmse = np.sqrt(np.mean((rt_kf[peaks].T - kf.data.rt[peaks])**2, axis=0))
+    else:
+        rt_kf = np.array([θ.beta * θ.t_I for θ in kf.θ_list])
+        rmse = np.sqrt(np.mean((rt_kf.T - kf.data.rt[:len(rt_kf)])**2, axis=0))
+
+    return np.mean(rmse)
