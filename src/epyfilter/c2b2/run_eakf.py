efcbce00212841832186e80265ed6809e8ef9516
Han Yong Wunrow <nhw2114@columbia.edu>
2023-10-28 13:53:49 -0400

    rmse at peaks for rt and rename csvs to match data files

--- src/epyfilter/c2b2/run_eakf.py
+++ src/epyfilter/c2b2/run_eakf.py
@@ -3,14 +3,13 @@ import argparse
 import numpy as np
 import pandas as pd
 import pickle
-from epyfilter import model_da, eakf, enks, posterior_checks
+from epyfilter import model_da, eakf, enks, posterior_checks, simulate_data
 
 from numpy.random import uniform
 
 
 if __name__ == '__main__':
-    # sge_task_id = int(os.environ.get("SGE_TASK_ID"))
-    sge_task_id = 1
+    sge_task_id = int(os.environ.get("SGE_TASK_ID"))
 
     parser = argparse.ArgumentParser(
         description="Run EAKF with adaptive, fixed, and no inflation for 1000 different synthetic data sets",
@@ -47,6 +46,8 @@ if __name__ == '__main__':
         pickle_files = df.iloc[start_row:, 0]
 
     for i, pickle_file in enumerate(pickle_files):
+        param_num = os.path.basename(pickle_file).split("_")[0]
+
         with open(pickle_file, 'rb') as file:
             data = pickle.load(file)
 
@@ -70,7 +71,11 @@ if __name__ == '__main__':
         late_day = -1/data.k * np.log((beta_1 - beta_0)/(0.99*beta_1 - beta_0)-1) + data.midpoint
         late_day = int(late_day)
 
-        check_array = np.empty((0, 5))
+        det_data = simulate_data.simulate_data(**data.true_params, run_deterministic=True)
+        peak_days, = np.where(np.diff(np.sign(np.diff(det_data.i_true))) == -2)
+
+        columns=["method", "rt_peak_rmse", "rt_rmse", "data_rmse","avg_w2","avg_kl","in_ci"]
+        check_df = pd.DataFrame(columns=columns)
 
         for r in range(args.n_real):
 
@@ -79,61 +84,74 @@ if __name__ == '__main__':
             kf.filter(prior)
             
             if r == 0:
-                kf.plot_posterior(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_posterior_adaptive_inflation")
-                kf.plot_reliability(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_reliability_adaptive_inflation")
-                kf.plot_ppc(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_ppc_adaptive_inflation")
+                kf.plot_posterior(path=args.out_dir, name=f"{param_num}_eakf_posterior_adaptive_inflation")
+                kf.plot_reliability(path=args.out_dir, name=f"{param_num}_eakf_reliability_adaptive_inflation")
+                kf.plot_ppc(path=args.out_dir, name=f"{param_num}_eakf_ppc_adaptive_inflation")
 
-            kf_checks = np.array(["adaptive inflation",
-                                  posterior_checks.rmse(kf),
-                                  posterior_checks.avg_wasserstein2(kf),
-                                  posterior_checks.avg_kl_divergence(kf),
-                                  posterior_checks.check_param_in_ci(kf, late_day)])
-            check_array = np.vstack((check_array, kf_checks))
+            kf_checks = pd.DataFrame([
+                ["adaptive inflation",
+                 posterior_checks.rt_rmse(kf, peaks=peak_days),
+                 posterior_checks.rt_rmse(kf),
+                 posterior_checks.data_rmse(kf),
+                 posterior_checks.avg_wasserstein2(kf),
+                 posterior_checks.avg_kl_divergence(kf),
+                 posterior_checks.check_param_in_ci(kf,late_day)]
+                 ], columns=columns)
+            check_df = pd.concat([check_df, kf_checks], ignore_index=True)
 
             # no inflation
             kf_no = eakf.EnsembleAdjustmentKalmanFilter(model, m=args.n_ens)
             kf_no.filter(prior, inf_method="none")
 
             if r == 0:
-                kf_no.plot_posterior(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_posterior_no_inflation")
-                kf_no.plot_reliability(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_reliability_no_inflation")
-                kf_no.plot_ppc(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_ppc_no_inflation")
+                kf_no.plot_posterior(path=args.out_dir, name=f"{param_num}_eakf_posterior_no_inflation")
+                kf_no.plot_reliability(path=args.out_dir, name=f"{param_num}_eakf_reliability_no_inflation")
+                kf_no.plot_ppc(path=args.out_dir, name=f"{param_num}_eakf_ppc_no_inflation")
 
-            kf_no_checks = np.array(["no inflation",
-                                     posterior_checks.rmse(kf_no),
-                                     posterior_checks.avg_wasserstein2(kf_no),
-                                     posterior_checks.avg_kl_divergence(kf_no),
-                                     posterior_checks.check_param_in_ci(kf_no, late_day)])
-            check_array = np.vstack((check_array, kf_no_checks))
+            kf_no_checks = pd.DataFrame([
+                ["no inflation",
+                 posterior_checks.rt_rmse(kf_no, peaks=peak_days),
+                 posterior_checks.rt_rmse(kf_no),
+                 posterior_checks.data_rmse(kf_no),
+                 posterior_checks.avg_wasserstein2(kf_no),
+                 posterior_checks.avg_kl_divergence(kf_no),
+                 posterior_checks.check_param_in_ci(kf_no,late_day)]
+                 ], columns=columns)
+            check_df = pd.concat([check_df, kf_no_checks], ignore_index=True)
 
             # fixed inflation 
             kf_fixed = eakf.EnsembleAdjustmentKalmanFilter(model, m=args.n_ens)
             kf_fixed.filter(prior, inf_method="constant", lam_fixed=args.fixed_inf)
             
             if r == 0:
-                kf_fixed.plot_posterior(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_posterior_fixed_inflation")
-                kf_fixed.plot_reliability(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_reliability_fixed_inflation")
-                kf_fixed.plot_ppc(path=args.out_dir, name=f"{sge_task_id}_{i}_eakf_ppc_fixed_inflation")
+                kf_fixed.plot_posterior(path=args.out_dir, name=f"{param_num}_eakf_posterior_fixed_inflation")
+                kf_fixed.plot_reliability(path=args.out_dir, name=f"{param_num}_eakf_reliability_fixed_inflation")
+                kf_fixed.plot_ppc(path=args.out_dir, name=f"{param_num}_eakf_ppc_fixed_inflation")
 
-            kf_fixed_checks = np.array(["fixed inflation",
-                                        posterior_checks.rmse(kf_fixed),
-                                        posterior_checks.avg_wasserstein2(kf_fixed),
-                                        posterior_checks.avg_kl_divergence(kf_fixed),
-                                        posterior_checks.check_param_in_ci(kf_fixed, late_day)])
-            check_array = np.vstack((check_array, kf_fixed_checks))
+            kf_fixed_checks = pd.DataFrame([
+                ["fixed inflation",
+                posterior_checks.rt_rmse(kf_fixed, peaks=peak_days),
+                posterior_checks.rt_rmse(kf_fixed),
+                posterior_checks.data_rmse(kf_fixed),
+                posterior_checks.avg_wasserstein2(kf_fixed),
+                posterior_checks.avg_kl_divergence(kf_fixed),
+                posterior_checks.check_param_in_ci(kf_fixed, late_day)]
+                ], columns=columns)
+            check_df = pd.concat([check_df, kf_fixed_checks], ignore_index=True)
 
             ks = enks.EnsembleSquareRootSmoother(kf)
             ks.smooth(window_size=10, plot=False)
 
-            ks_checks = np.array(["smooth",
-                                  posterior_checks.rmse(ks),
-                                  posterior_checks.avg_wasserstein2_ks(ks),
-                                  posterior_checks.avg_kl_divergence_ks(ks),
-                                  posterior_checks.check_param_in_ci_ks(ks, late_day)])
-            check_array = np.vstack((check_array, ks_checks))
+            ks_checks = pd.DataFrame([
+                ["smooth",
+                 posterior_checks.rt_rmse(ks, peaks=peak_days),
+                 posterior_checks.rt_rmse(ks),
+                 posterior_checks.data_rmse(ks),
+                 posterior_checks.avg_wasserstein2_ks(ks),
+                 posterior_checks.avg_kl_divergence_ks(ks),
+                 posterior_checks.check_param_in_ci_ks(ks, late_day)]
+                ], columns=columns)
+            check_df = pd.concat([check_df, ks_checks], ignore_index=True)
         
-        check_df = pd.DataFrame(check_array, columns=["method","rmse","avg_w2","avg_kl","in_ci"])
-        import pdb; pdb.set_trace()
         check_df = check_df.groupby("method").mean()
-
-        check_df.to_csv(f"args.out_dir/{i}_{sge_task_id}.csv", index=False)
+        check_df.to_csv(f"{args.out_dir}/{param_num}_eakf_metrics.csv", index=True)
