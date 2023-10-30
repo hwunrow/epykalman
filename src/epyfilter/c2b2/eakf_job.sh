9ce59b577cfa210469e79e8cf9cf26f412b5d0e1
Han Yong Wunrow <nhw2114@columbia.edu>
2023-10-30 00:56:24 -0400

    longer runtime

--- src/epyfilter/c2b2/eakf_job.sh
+++ src/epyfilter/c2b2/eakf_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l mem=10G,time=1:: -S /bin/bash -N eakf
+#$ -l mem=10G,time=20:: -S /bin/bash -N eakf
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/run_eakf.py \
