cf8d258931bdc8b7f54a67a868309674fbcc4fde
Han Yong Wunrow <nhw2114@columbia.edu>
2023-11-19 22:41:56 -0500

    don't save epiestim draws, just CIs, mean, and median

--- src/epyfilter/c2b2/epiestim_parallel.R
+++ src/epyfilter/c2b2/epiestim_parallel.R
@@ -108,7 +108,23 @@ for (file in files) {
   }
   
   R_posterior_all_dt <- rbindlist(dt_list)
-  fwrite(R_posterior_all_dt, file=paste0(out_dir,"/",param_num, "_epiEstim_draws.csv"))
+  # 95% CI
+  percentile <- 95
+  quantiles <- c((1 - percentile/100)/2, 1 - (1 - percentile/100)/2)
+  percentile95_dt <- dt[,as.list(quantile(.SD, quantiles, na.rm=TRUE)),  .SDcols=paste0("sample", 1:300), by=.(day,window)]
+  # 50% CI
+  percentile <- 50
+  quantiles <- c((1 - percentile/100)/2, 1 - (1 - percentile/100)/2)
+  percentile50_dt <- dt[,as.list(quantile(.SD, quantiles, na.rm=TRUE)),  .SDcols=paste0("sample", 1:300), by=.(day,window)]
+  # mean
+  mean_dt <- dt[, .(mean = rowMeans(.SD, na.rm=TRUE)), .SDcols=paste0("sample", 1:300), by=.(day,window)]
+  # median
+  med_dt <- dt[, .(med = rowMedians(as.matrix(.SD), na.rm=TRUE)), .SDcols=paste0("sample", 1:300), by=.(day,window)]
+  
+  merge_dt <- merge(percentile95_dt, percentile50_dt, by=c("window","day"))
+  merge_dt <- merge(merge_dt, mean_df, by=c("window","day"))
+  merge_dt <- merge(merge_dt, med_dt, by=c("window","day"))
+  fwrite(merge_dt, file=paste0(out_dir,"/",param_num, "_epiEstim_for_plot.csv"))
   
   # posterior checks
   data_rmse_result <- data_rmse(R_posterior_all_dt, synthetic_dt)
