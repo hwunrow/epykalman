923a4e9938dd4b36815971b883f41aa159884a64
Han Yong Wunrow <nhw2114@columbia.edu>
2023-11-21 11:59:52 -0500

    increase time to 2 hours

--- src/epyfilter/c2b2/epiestim_job.sh
+++ src/epyfilter/c2b2/epiestim_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l mem=5G,time=1:: -S /bin/bash -N epiestim
+#$ -l mem=5G,time=2:: -S /bin/bash -N epiestim
 module load singularity
 PASSWORD='ihme123' singularity exec --bind "$HOME"/run:/run,"$HOME"/var-lib-rstudio-server:/var/lib/rstudio-server,"$HOME"/database.conf:/etc/rstudio/database.conf,/ifs/scratch/jls106_gp/nhw2114:/ifs/scratch/jls106_gp/nhw2114 \
     "$HOME"/rstudio.simg Rscript --no-save --vanilla /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/epiestim_parallel.R \
