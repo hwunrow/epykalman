9723b4f3dc5e8536f4ea33c929aab24bf1c04569
Han Yong Wunrow <nhw2114@columbia.edu>
2024-01-29 15:24:25 -0500

    shell scripts

--- src/epyfilter/c2b2/eakf_for_plots.sh
+++ src/epyfilter/c2b2/eakf_for_plots.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#$ -l mem=20G,time=5:: -S /bin/bash -N eakf
+#$ -l mem=20G,time=10:: -S /bin/bash -N eakf
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/run_eakf.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
-    --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \ 
+    --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
     --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
-    --param-list 2236   3457   6115  16729  21258  55685  58428  63679 69090  70286  73071  85084  96475  96799  98225  99368 100184 100185  21258  55685  58428  63679  69090  70286 73071  85084  96475  96799  98225  99368 100184 100185 46482  66815  43056   5863      1   3348  45193   8019 8298   3775  27533  28485  40299  84882  89399  57066 49477  21258  43775  55741  36206
+    --param-list 1 3348 3775 5863 8019 8298 27533 28485 36206 40299 43056 43775 45193 46482 49477 55741 57066 66815 84882 89399
--- src/epyfilter/c2b2/epiestim_job.sh
+++ src/epyfilter/c2b2/epiestim_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -l mem=5G,time=3:: -S /bin/bash -N epiestim
+#$ -l mem=20G,time=10:: -S /bin/bash -N epiestim
 module load singularity
 PASSWORD='ihme123' singularity exec --bind "$HOME"/run:/run,"$HOME"/var-lib-rstudio-server:/var/lib/rstudio-server,"$HOME"/database.conf:/etc/rstudio/database.conf,/ifs/scratch/jls106_gp/nhw2114:/ifs/scratch/jls106_gp/nhw2114 \
     "$HOME"/rstudio.simg Rscript --no-save --vanilla /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/epiestim_parallel.R \
-    --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ --data-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data/ --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20231106_synthetic_data/ \
-    --param-list 1465 1497 1505 1513 1545 1553 1561 2692 33516 607 609 610 612 613 62711 659 688
+    --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ --data-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/ --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/ \
+    --param-list 1   2236   3348   3457   3775   5863   6115   8019 8298  16729  21258  27533  28485  36206  40299  43056 43775  45193  46482  49477  55685  55741  57066  58428 63679  66815  69090  70286  73071  84882  85084  89399 96475  96799  98225  99368 100184 100185
