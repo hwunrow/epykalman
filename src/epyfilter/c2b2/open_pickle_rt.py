262f3d4cd2dd8ab0b1f4f053e23ff755ae845775
Han Yong Wunrow <nhw2114@columbia.edu>
2024-01-29 15:22:30 -0500

    add param list argument to pickling

--- src/epyfilter/c2b2/open_pickle_rt.py
+++ src/epyfilter/c2b2/open_pickle_rt.py
@@ -6,29 +6,32 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 
+
 def open_pickle(pickle_file):
-  with open(pickle_file, 'rb') as file:
-    data = pickle.load(file)
+    with open(pickle_file, "rb") as file:
+        data = pickle.load(file)
 
-  return data
+    return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         sge_task_id = int(os.environ.get("SGE_TASK_ID"))
-        sge_outputs_file = os.environ.get('SGE_STDOUT_PATH')
+        sge_outputs_file = os.environ.get("SGE_STDOUT_PATH")
         files_per_task = 1000
     except:
         sge_task_id = 1
         sge_outputs_file = "test.log"
         files_per_task = 100_185
-    
-    logger = logging.getLogger('my_logger')
+
+    logger = logging.getLogger("my_logger")
     logger.setLevel(logging.INFO)
 
     sg_outputs_handler = logging.FileHandler(sge_outputs_file)
     sg_outputs_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     sg_outputs_handler.setFormatter(formatter)
     logger.addHandler(sg_outputs_handler)
 
@@ -36,24 +39,39 @@ if __name__ == '__main__':
         description="Run EAKF with adaptive, fixed, and no inflation for 1000 different synthetic data sets",
     )
     parser.add_argument(
-        "--in-dir", type=str, required=True,
-        help="Directory for inputs.")
+        "--in-dir", type=str, required=True, help="Directory for inputs."
+    )
     parser.add_argument(
-        "--out-dir", type=str, required=True,
-        help="Directory to save plots and files.")
+        "--out-dir", type=str, required=True, help="Directory to save plots and files."
+    )
+    parser.add_argument(
+        "--param-list", type=int, nargs="+", help="Rerunning for specific sge_task_ids"
+    )
     args = parser.parse_args()
 
     df = pd.read_csv(os.path.join(args.in_dir, "pickle_list.csv"))
     start_row = (sge_task_id - 1) * files_per_task
     end_row = sge_task_id * files_per_task
     if end_row < len(df):
-      pickle_files = df.iloc[start_row:end_row, 0]
+        pickle_files = df.iloc[start_row:end_row, 0]
     else:
-      pickle_files = df.iloc[start_row:, 0]
+        pickle_files = df.iloc[start_row:, 0]
+
+    # override if supplied param list
+    if args.param_list:
+        pickle_files = [
+            f"/ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/{pp}_synthetic_data.pkl"
+            for pp in args.param_list
+        ]
 
     for i, pickle_file in enumerate(tqdm(pickle_files)):
-      data = open_pickle(pickle_file)
-      param_num = os.path.basename(pickle_file).split("_")[0]
-      tmp_df = pd.DataFrame(np.stack([data.rt,data.i[1:], data.S[1:]/data.N]).T, columns=["rt","i","prop_S"])
-      tmp_df.to_csv(f"{args.out_dir}/{param_num}_for_epiestim.csv", index=False)
-      logger.info(param_num)
+        data = open_pickle(pickle_file)
+        param_num = os.path.basename(pickle_file).split("_")[0]
+        tmp_df = pd.DataFrame(
+            np.stack([data.rt, data.i[1:], data.S[1:] / data.N]).T,
+            columns=["rt", "i", "prop_S"],
+        )
+        tmp_df.to_csv(f"{args.out_dir}/{param_num}_for_epiestim.csv", index=False)
+        logger.info(param_num)
+
+    logger.info("Done!")
--- src/epyfilter/c2b2/pickle_job.sh
+++ src/epyfilter/c2b2/pickle_job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#$ -l mem=5G,time=1:: -S /bin/bash -N pickle
+#$ -l mem=10G,time=10:: -S /bin/bash -N pickle
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/open_pickle_rt.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
-    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data
+    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
+    --param-list 1   2236   3348   3457   3775   5863   6115   8019 8298  16729  21258  27533  28485  36206  40299  43056 43775  45193  46482  49477  55685  55741  57066  58428 63679  66815  69090  70286  73071  84882  85084  89399 96475  96799  98225  99368 100184 100185
