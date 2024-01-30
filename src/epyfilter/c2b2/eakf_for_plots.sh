9135a475e05056fb1092356a19511db6abc21413
Han Yong Wunrow <nhw2114@columbia.edu>
2024-01-30 15:57:34 -0500

    min param nums for eakf shell script

--- src/epyfilter/c2b2/eakf_for_plots.sh
+++ src/epyfilter/c2b2/eakf_for_plots.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#$ -l mem=20G,time=10:: -S /bin/bash -N eakf
+#$ -l mem=20G,time=5:: -S /bin/bash -N eakf_plots
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/run_eakf.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
-    --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
+    --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data \
     --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
-    --param-list 1 3348 3775 5863 8019 8298 27533 28485 36206 40299 43056 43775 45193 46482 49477 55741 57066 66815 84882 89399
+    --param-list 2 96956 60997 97638 4616 44331 99403 26094 57072 60945 1337 190 54589 32062
