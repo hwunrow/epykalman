02dfbe041f4e9f0abfdf8ba82e02825b4864de84
Han Yong Wunrow <nhw2114@columbia.edu>
2024-02-05 21:33:43 -0500

    edit shell scripts for resubmiting example params for plots

--- src/epyfilter/c2b2/epiestim_job.sh
+++ src/epyfilter/c2b2/epiestim_job.sh
@@ -4,4 +4,4 @@ module load singularity
 PASSWORD='ihme123' singularity exec --bind "$HOME"/run:/run,"$HOME"/var-lib-rstudio-server:/var/lib/rstudio-server,"$HOME"/database.conf:/etc/rstudio/database.conf,/ifs/scratch/jls106_gp/nhw2114:/ifs/scratch/jls106_gp/nhw2114 \
     "$HOME"/rstudio.simg Rscript --no-save --vanilla /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/epiestim_parallel.R \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ --data-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/ --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/ \
-    --param-list 1   2236   3348   3457   3775   5863   6115   8019 8298  16729  21258  27533  28485  36206  40299  43056 43775  45193  46482  49477  55685  55741  57066  58428 63679  66815  69090  70286  73071  84882  85084  89399 96475  96799  98225  99368 100184 100185
+    --param-list 2 96956 60997 97638 4616 44331 99403 26094 57072 60945 1337 190 54589 32062
