8a505e9b0181013f0f38289ca9802cdb01224ee8
Han Yong Wunrow <nhw2114@columbia.edu>
2024-02-12 23:36:24 -0500

    shell script for 20240212_run

--- src/epyfilter/c2b2/eakf_resubmit_job.sh
+++ src/epyfilter/c2b2/eakf_resubmit_job.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#$ -l mem=5G,time=5:: -S /bin/bash -N eakf
+#$ -l mem=20G,time=15:: -S /bin/bash -N eakf -t 1-958
+#$ -e /ifs/scratch/jls106_gp/nhw2114/data/20240212_run
+#$ -o /ifs/scratch/jls106_gp/nhw2114/data/20240212_run
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/run_eakf.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
-    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20231106_synthetic_data
+    --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data \
+    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20240212_run \
+    --save-reliability
