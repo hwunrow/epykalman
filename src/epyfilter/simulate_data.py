750c7428d001c22b1c2b95d4d06c019429538249
Han Yong Wunrow <nhw2114@columbia.edu>
2024-02-20 10:40:49 -0500

    flake8

--- src/epyfilter/simulate_data.py
+++ src/epyfilter/simulate_data.py
@@ -5,11 +5,21 @@ import inspect
 import logging
 
 
-class simulate_data():
+class simulate_data:
     def __init__(
-            self, rt_0, rt_1, midpoint, k, n_t, t_I, N, S0, I0,
-            run_deterministic=False, **kwargs
-            ):
+        self,
+        rt_0,
+        rt_1,
+        midpoint,
+        k,
+        n_t,
+        t_I,
+        N,
+        S0,
+        I0,
+        run_deterministic=False,
+        **kwargs,
+    ):
         r"""
         Args:
           rt_0 (float): Rt value before midpoint
@@ -35,17 +45,17 @@ class simulate_data():
         if isinstance(I0, float):
             logging.info("Casting I0 to int")
             I0 = int(I0)
-        
+
         self.true_params = {
-                'rt_0': rt_0,
-                'rt_1': rt_1,
-                'midpoint': midpoint,
-                'k': k,
-                'n_t': n_t,
-                't_I': t_I,
-                'N': N,
-                'S0': S0,
-                'I0': I0,
+            "rt_0": rt_0,
+            "rt_1": rt_1,
+            "midpoint": midpoint,
+            "k": k,
+            "n_t": n_t,
+            "t_I": t_I,
+            "N": N,
+            "S0": S0,
+            "I0": I0,
         }
         self.rt_0 = rt_0
         self.rt_1 = rt_1
@@ -63,20 +73,22 @@ class simulate_data():
 
         if run_deterministic:
             self.S, self.I, self.R, self.i = self.gen_sir_det(
-                self.rt, t_I, N, S0, I0, n_t)
+                self.rt, t_I, N, S0, I0, n_t
+            )
             self.i_true = self.i
         else:
             self.S, self.I, self.R, self.i, self.i_true = self.gen_sir_stoch(
-                self.rt, t_I, N, S0, I0, n_t, **kwargs)
+                self.rt, t_I, N, S0, I0, n_t, **kwargs
+            )
 
     def sigmoid(self, rt_0, rt_1, midpoint, k, n_t):
         """Computes sigmoid curve"""
         t = np.arange(0, n_t)
-        sigmoid = rt_0 + (rt_1 - rt_0) / (1 + np.exp(-k*(t - midpoint)))
+        sigmoid = rt_0 + (rt_1 - rt_0) / (1 + np.exp(-k * (t - midpoint)))
         return sigmoid
 
     def construct_beta(self, rt, t_I):
-        self.beta = rt/t_I
+        self.beta = rt / t_I
         return rt / t_I
 
     def gen_sir_det(self, rt, t_I, N, S0, I0, n_t):
@@ -86,12 +98,12 @@ class simulate_data():
         R = np.array([0])
         i = np.array([0])
         for t in range(n_t):
-            dSI = beta[t]*Ir[t]*S[t]/N
-            dIR = Ir[t]/t_I
+            dSI = beta[t] * Ir[t] * S[t] / N
+            dIR = Ir[t] / t_I
 
-            S_new = np.clip(S[t]-dSI, 0, N)
-            I_new = np.clip(Ir[t]+dSI-dIR, 0, N)
-            R_new = np.clip(R[t]+dIR, 0, N)
+            S_new = np.clip(S[t] - dSI, 0, N)
+            I_new = np.clip(Ir[t] + dSI - dIR, 0, N)
+            R_new = np.clip(R[t] + dIR, 0, N)
 
             S = np.append(S, S_new)
             Ir = np.append(Ir, I_new)
@@ -101,21 +113,20 @@ class simulate_data():
         return S, Ir, R, i
 
     def gen_sir_stoch(
-            self, rt, t_I, N, S0, I0, n_t,
-            add_noise=False, noise_param=1/25
-            ):
+        self, rt, t_I, N, S0, I0, n_t, add_noise=False, noise_param=1 / 25
+    ):
         beta = self.construct_beta(rt, t_I)
         S = np.array([S0])
         Ir = np.array([I0])
         R = np.array([0])
         i = np.array([0])
         for t in range(n_t):
-            dSI = np.random.poisson(beta[t]*Ir[t]*S[t]/N)
-            dIR = np.random.poisson(Ir[t]/t_I)
+            dSI = np.random.poisson(beta[t] * Ir[t] * S[t] / N)
+            dIR = np.random.poisson(Ir[t] / t_I)
 
-            S_new = np.clip(S[t]-dSI, 0, N)
-            I_new = np.clip(Ir[t]+dSI-dIR, 0, N)
-            R_new = np.clip(R[t]+dIR, 0, N)
+            S_new = np.clip(S[t] - dSI, 0, N)
+            I_new = np.clip(Ir[t] + dSI - dIR, 0, N)
+            R_new = np.clip(R[t] + dIR, 0, N)
 
             S = np.append(S, S_new)
             Ir = np.append(Ir, I_new)
@@ -124,9 +135,9 @@ class simulate_data():
 
         i_true = i
         if add_noise:
-            i = i.astype('float64')
+            i = i.astype("float64")
             self.noise_param = noise_param
-            obs_error_var = np.maximum(1., i[1:]**2 * noise_param)
+            obs_error_var = np.maximum(1.0, i[1:] ** 2 * noise_param)
             obs_error_sample = np.random.normal(0, 1, size=n_t)
             i[1:] += obs_error_sample * np.sqrt(obs_error_var)
             i = np.clip(i, 0, N)
@@ -137,8 +148,7 @@ class simulate_data():
         data_distribution = np.zeros(shape=self.i.shape)
 
         for _ in range(num_real):
-            a = simulate_data(
-                **self.true_params, add_noise=True, noise_param=1/50)
+            a = simulate_data(**self.true_params, add_noise=True, noise_param=1 / 50)
             data_distribution = np.vstack((data_distribution, a.i))
 
         data_distribution = np.delete(data_distribution, (0), axis=0)
@@ -147,36 +157,37 @@ class simulate_data():
     def plot_rt(self, ax=None):
         if not ax:
             fig, ax = plt.subplots()
-        ax.plot(self.rt, '.-')
-        ax.set_title('Rt')
+        ax.plot(self.rt, ".-")
+        ax.set_title("Rt")
 
     def plot_SIR(self, ax=None):
         if not ax:
             fig, ax = plt.subplots()
-        ax.plot(self.S, '.-', label='S')
-        ax.plot(self.I, '.-', label='I')
-        ax.plot(self.R, '.-', label='R')
-        ax.set_title('Stochastic SIR')
+        ax.plot(self.S, ".-", label="S")
+        ax.plot(self.I, ".-", label="I")
+        ax.plot(self.R, ".-", label="R")
+        ax.set_title("Stochastic SIR")
         ax.legend()
 
     def plot_obs(self, ax=None):
         if not ax:
             fig, ax = plt.subplots()
-        ax.plot(self.i, '.')
-        ax.set_title('Stochastic Daily Case Counts')
+        ax.plot(self.i, ".")
+        ax.set_title("Stochastic Daily Case Counts")
 
     def plot_moving_average(self, ax=None):
         def _moving_average(a, n=7):
             ret = np.cumsum(a, dtype=float)
             ret[n:] = ret[n:] - ret[:-n]
-            return ret[n - 1:] / n
+            return ret[n - 1 :] / n
+
         if not ax:
             fig, ax = plt.subplots()
         ax.plot(_moving_average(np.diff(self.i, n=1), n=7))
-        ax.hlines(0, 0, 365, color='red')
-        ax.vlines(100, -160, 100, color='red')
+        ax.hlines(0, 0, 365, color="red")
+        ax.vlines(100, -160, 100, color="red")
 
-    def plot_all(self, path=None, name='synthetic_plots'):
+    def plot_all(self, path=None, name="synthetic_plots"):
         fig, axs = plt.subplots(2, 2, sharex=True, figsize=(10, 10))
         self.plot_rt(axs[0, 0])
         self.plot_SIR(axs[0, 1])
@@ -184,11 +195,11 @@ class simulate_data():
         fig.delaxes(axs[1, 1])
 
         if path:
-            plt.savefig(f'{path}/{name}.pdf')
+            plt.savefig(f"{path}/{name}.pdf")
 
-    def save_data(self, path=None, name='synthetic_data'):
+    def save_data(self, path=None, name="synthetic_data"):
         # log source code
         lines = inspect.getsource(simulate_data)
         logging.info(lines)
-        with open(f'{path}/{name}.pkl', 'wb') as file:
+        with open(f"{path}/{name}.pkl", "wb") as file:
             pickle.dump(self, file, pickle.HIGHEST_PROTOCOL)
