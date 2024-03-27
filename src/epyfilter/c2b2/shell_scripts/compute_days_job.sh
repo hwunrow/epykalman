60938e846ab03eff9c9273ae6044f6f717b423c4
Han Yong Wunrow <nhw2114@columbia.edu>
2024-03-27 14:18:35 -0400

    rerun data distribution since I accidentally deleted some

--- src/epyfilter/c2b2/shell_scripts/compute_days_job.sh
+++ src/epyfilter/c2b2/shell_scripts/compute_days_job.sh
@@ -5,4 +5,5 @@ conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/compute_days.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
     --synthetic-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data \
-    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20231106_synthetic_data
+    --out-dir /ifs/scratch/jls106_gp/nhw2114/data/20231106_synthetic_data \
+    --compute-dd
