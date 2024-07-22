fb15c226b2516368e7cc11dbfab3a10bc10afa6f
Han Yong Wunrow <nhw2114@columbia.edu>
2024-07-21 23:37:21 -0400

    shell script for final version of runs for plotting, check either param list or file is provided in run_eakf.py

--- src/epyfilter/c2b2/run_eakf.py
+++ src/epyfilter/c2b2/run_eakf.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
         "--out-dir", type=str, required=True, help="Directory to save plots and files."
     )
     parser.add_argument(
-        "--param-file", type=str, required=True, help="CSV filename with params to run."
+        "--param-file", type=str, required=False, help="CSV filename with params to run."
     )
     parser.add_argument(
         "--param-list", type=int, nargs="+", help="Rerunning for specific sge_task_ids"
@@ -210,18 +210,22 @@ if __name__ == "__main__":
 
     np.random.seed(1994)
 
-    files_per_task = args.files_per_task
-    df = pd.read_csv(os.path.join(args.in_dir, args.param_file))
-    start_row = (sge_task_id - 1) * files_per_task
-    end_row = sge_task_id * files_per_task
-    if end_row < len(df):
-        pickle_files = df.iloc[start_row:end_row, 0]
-    else:
-        pickle_files = df.iloc[start_row:, 0]
+    if not args.param_file and not args.param_list:
+        raise ValueError("Must supply either param_file or param list")
 
-    pickle_files = [
-        os.path.join(args.pkl_dir, f"{p}_synthetic_data.pkl") for p in pickle_files
-    ]
+    if args.param_file:
+        files_per_task = args.files_per_task
+        df = pd.read_csv(os.path.join(args.in_dir, args.param_file))
+        start_row = (sge_task_id - 1) * files_per_task
+        end_row = sge_task_id * files_per_task
+        if end_row < len(df):
+            pickle_files = df.iloc[start_row:end_row, 0]
+        else:
+            pickle_files = df.iloc[start_row:, 0]
+
+        pickle_files = [
+            os.path.join(args.pkl_dir, f"{p}_synthetic_data.pkl") for p in pickle_files
+        ]
     # override if supplied param list
     if args.param_list:
         pickle_files = [
--- src/epyfilter/c2b2/shell_scripts/eakf_for_plots.sh
+++ src/epyfilter/c2b2/shell_scripts/eakf_for_plots.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#$ -l mem=20G,time=5:: -S /bin/bash -N eakf_plots
+#$ -l mem=20G,time=5:: -S /bin/bash -N eakf_plots -t 1-5
+#$ -e /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper
+#$ -o /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper
 source ~/.bashrc
 conda activate epyfilter
 python /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/run_eakf.py \
     --in-dir /ifs/scratch/jls106_gp/nhw2114/repos/rt-estimation/src/epyfilter/c2b2/ \
     --pkl-dir /ifs/scratch/jls106_gp/nhw2114/data/20231025_synthetic_data \
     --out-dir /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper \
-    --param-list 2 96956 60997 97638 4616 44331 99403 26094 57072 60945 1337 190 54589 32062
+    --save-data \
+    --save-reliability \
+    --param-file /ifs/scratch/jls106_gp/nhw2114/data/example_plots_for_paper/plot_params.csv \
+    --files-per-task 10
