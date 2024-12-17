514ee2e87c9e34bb5c27c1354673dbe1c12643e9
Han Yong Wunrow <nhw2114@columbia.edu>
2024-12-17 04:22:24 -0500

    add optional argument for prior on state variables

--- src/epyfilter/eakf/model_da.py
+++ src/epyfilter/eakf/model_da.py
@@ -21,7 +21,7 @@ class SIR_model():
     )
     ModelParams.__qualname__ = 'SIR_model.ModelParams'
 
-    def __init__(self, data):
+    def __init__(self, data, prior=None):
         """
         data: simulate_data
         """
@@ -34,6 +34,7 @@ class SIR_model():
             i=0
         )
         self.θ = self.ModelParams(beta, data.t_I)
+        self.prior = prior
 
     def f(self, t, x, θ, N, dt=1, noise_param=1/25):
         """
@@ -79,15 +80,24 @@ class SIR_model():
             Args:
                 pop: population
                 m: number of ensemble members
+                pior: prior
         """
-        #     I0 = pop * inf_init
-        #     S0 = pop - I0
-        #     R0 = 0
-        #     i = 0
-        S0 = np.random.uniform(pop*0.8, pop, size=m)
-        I0 = pop - S0
-        R0 = np.zeros(m)
-        i0 = np.zeros(m)
+        if self.prior is None:
+            S0 = np.random.uniform(pop*0.8, pop, size=m)
+            I0 = pop - S0
+            R0 = np.zeros(m)
+            i0 = np.zeros(m)
+        else:
+            S0 = self.prior['S']['dist'](**self.prior['S']['args'], size=m) * pop
+            I0 = self.prior['I']['dist'](**self.prior['I']['args'], size=m) * pop
+            R0 = pop - S0 - I0
+            R0 = np.maximum(R0, 0)
+            i0 = np.zeros(m)
+
+            total = S0 + I0 + R0
+            S0 = S0 * pop / total
+            I0 = I0 * pop / total
+            R0 = R0 * pop / total 
 
         x0 = self.SIR(
             S=S0,
